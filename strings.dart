22e712f91f27671b5843caf312031386e4f45844
Danniel Navas <dannielnavas@gmail.com>
2024-03-08 07:22:32 -0500

    ✨ Agregado archivo strings.dart con cambios en concatenación de strings, interpolación y métodos toUppercase, toLowerCase y replaceAll.

---
+++ strings.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Esto es un comentario
+  /**
+    Bloque comentado
+   */
+
+   // Concatenación de string
+  String name = "Danny";
+  String greeting = "Hello, ";
+  String message = greeting + name;
+  print(message); // Output: Hello, Danny
+
+  // interpolación
+  String message = "$greeting "\$name \"  \n y vivo en  \t Colombia";
+
+  // toUppercase, toLowerCase y replaceAll
+
+  print(name.toLowerCase());
+  print(name.toUppercase());
+  print(message.replaceAll("Colombia", "Mexico"));
+
+}
