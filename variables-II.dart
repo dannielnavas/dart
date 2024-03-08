9ac25a8c4f40e728d2615ac3de406a39e7b57510
Danniel Navas <dannielnavas@gmail.com>
2024-03-08 07:13:22 -0500

    ✨ Agrega ejemplos de operadores de incremento y decremento en variables

---
+++ variables-II.dart
@@ -0,0 +1,10 @@
+void main() {
+    int edad1 = 12;
+    int edad2 = edad1++; // primero asignación despues incremento
+    int edad2 = ++edad1; // primero incremento despues asignacion
+    int edad2 = edad1--;
+    int edad2 --edar1;
+
+    print(edad1);
+    print(edad2)
+}
