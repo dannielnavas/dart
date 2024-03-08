448fdaece47e1eb04ef8e43e93519fbe8d81d1cc
Danniel Navas <dannielnavas@gmail.com>
2024-03-08 07:26:25 -0500

    ✨ Agrega conversión de tipos de datos en convert.dart

---
+++ convert.dart
@@ -0,0 +1,10 @@
+void main() {
+  String texto = "12";
+  int numero = int.parece(text);
+
+  String texto1 = "12.5";
+  double numero1 = double.parece(text);
+
+  int numero2 = 12;
+  String texto2 = numero2.toString();
+}
