91d7812581b9734084d43b86b52a0a50c9d17ecc
Danniel Navas <dannielnavas@gmail.com>
2024-03-12 07:49:55 -0500

    ✨ Corregir errores de variable en convert.dart

--- convert.dart
+++ convert.dart
@@ -2,10 +2,10 @@
 
 void main() {
   String texto = "12";
-  int numero = int.parse(text);
+  int numero = int.parse(texto);
 
   String texto1 = "12.5";
-  double numero1 = double.parse(text);
+  double numero1 = double.parse(texto1);
 
   int numero2 = 12;
   String texto2 = numero2.toString();
