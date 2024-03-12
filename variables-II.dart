73e05eb6d81ec7dde8accb90620ed86171e3cc07
Danniel Navas <dannielnavas@gmail.com>
2024-03-12 07:50:14 -0500

    ✨ Actualizar variables-II.dart con cambios en las asignaciones y operaciones de incremento y decremento de edad

--- variables-II.dart
+++ variables-II.dart
@@ -1,10 +1,13 @@
 void main() {
-    int edad1 = 12;
-    int edad2 = edad1++; // primero asignación despues incremento
-    int edad2 = ++edad1; // primero incremento despues asignacion
-    int edad2 = edad1--;
-    int edad2 --edar1;
+  int edad1 = 12;
+  int edad2 = edad1++; // primero asignación despues incremento
+  int edad3 = ++edad1; // primero incremento despues asignacion
+  int edad4 = edad1--;
+  int edad5 = --edad1;
 
-    print(edad1);
-    print(edad2)
+  print(edad1);
+  print(edad2);
+  print(edad3);
+  print(edad4);
+  print(edad5);
 }
